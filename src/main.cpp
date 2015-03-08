cd8ba7216ba2ac6114849857c05bdc57b64827e8
Michael Layzell <michael@thelayzells.com>
2015-03-08 15:12:18 -0400

    First work on ray-based 4d grid traversal

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,125 @@
 #include "vertShader.h"
 
 #include "gl.h"
+#include <glm/glm.hpp>
 
 #include <stdlib.h>
 #include <iostream>
+
+using namespace glm;
+
+#define dumpVec4(v4) std::cout << v4.x << ", "\
+  << v4.y << ", "\
+  << v4.z << ", "\
+  << v4.w << "\n";
+
+// Fills path with count cells in 4d space which are touched by the vector starting
+// at start and passing in the direction dir
+// Based on http://stackoverflow.com/questions/11694886/traverse-a-2-5d-grid
+void gridIntersectPoints(vec4 start, vec4 dir, ivec4 *path, size_t count) {
+  ivec4 dirSigns(dir.x < 0 ? -1 : 1,
+                 dir.y < 0 ? -1 : 1,
+                 dir.z < 0 ? -1 : 1,
+                 dir.w < 0 ? -1 : 1);
+
+  vec4 pos = start;
+
+  while (count--) {
+    // Get the current cell
+    ivec4 posInt(pos);
+    *path++ = posInt;
+
+    // The value gotten by moving to one block further in each dimension
+    vec4 targets = posInt + dirSigns;
+    // The distance which must be traveled in each direction to get this
+    vec4 deltas = targets - pos;
+    // How many times dir must be traveled to cover this distance
+    vec4 deltaInDirs = deltas / dir;
+
+    // Choose the smallest one
+    float minDeltaInDirs = min(min(deltaInDirs.x, deltaInDirs.y),
+                               min(deltaInDirs.z, deltaInDirs.w));
+
+    // A small fudge to make sure that the next block is actually entred
+    // in case floating point errors cause the same block to be tested repeatedly
+    minDeltaInDirs += 1.0/16.0;
+
+    // Move along minDeltaInDirs!
+    pos += minDeltaInDirs * dir;
+  }
+
+
+
+  /* float maxDir = max(max(dir.x, dir.y),
+                     max(dir.z, dir.w));
+
+  vec4 sdir = dir/maxDir;
+  dumpVec4(dir);
+
+  vec4 pos = start;
+
+  while (count--) {
+    *path++ = pos;
+    pos += sdir;
+    } */
+
+  /* const float gridResolution = 1;
+
+  dir = normalize(dir);
+
+  dumpVec4(dir)
+
+  vec4 delta(gridResolution/fabs(dir.x),
+             gridResolution/fabs(dir.y),
+             gridResolution/fabs(dir.z),
+             gridResolution/fabs(dir.w));
+
+  dumpVec4(delta);
+
+  // Truncate and scale
+  ivec4 startGrid = start / gridResolution;
+  ivec4 currentGrid = startGrid;
+
+  dumpVec4(currentGrid);
+
+  // The step amount
+  ivec4 step(dir.x<0 ? -1 : 1,
+             dir.y<0 ? -1 : 1,
+             dir.z<0 ? -1 : 1,
+             dir.w<0 ? -1 : 1);
+
+  // TODO(michael): What?
+  vec4 cd(((step.x>0?start.x:start.x+1)*gridResolution-start.x)/dir.x,
+          ((step.y>0?start.y:start.y+1)*gridResolution-start.y)/dir.y,
+          ((step.z>0?start.z:start.z+1)*gridResolution-start.z)/dir.z,
+          ((step.w>0?start.w:start.w+1)*gridResolution-start.w)/dir.w);
+
+  std::cout << (step.z>0?start.z:start.z+1)*gridResolution-start.z;
+
+  std::cout << cd.x << ", "
+            << cd.y << ", "
+            << cd.z << ", "
+            << cd.w << "\n";
+
+  while (count--) {
+    *path = currentGrid;
+    path++;
+
+    if (cd.x < cd.y && cd.x < cd.z && cd.x < cd.w) {
+      cd.x += delta.x;
+      currentGrid.x += step.x;
+    } else if (cd.y < cd.z && cd.y < cd.w) {
+      cd.y += delta.y;
+      currentGrid.y += step.y;
+    } else if (cd.z < cd.w) {
+      cd.z += delta.z;
+      currentGrid.z += step.z;
+    } else {
+      cd.w += delta.w;
+      currentGrid.w += step.w;
+    }
+    } */
+}
 
 struct View {
   GLuint VAO;
@@ -132,6 +248,19 @@ int main(int argc, char **argv)
   worldTex.bindToUniform(0, gp.program_id, "world");
 
   glErrChk("After WorldTex");
+
+
+  // ___ TEST___
+  std::cout << "HERE\n";
+  ivec4 gips[10];
+  gridIntersectPoints(vec4(0,0,0,0), vec4(0.5,1,0,0), gips, 10);
+  for (size_t i=0; i<10; i++) {
+    std::cout << gips[i].x << ", "
+              << gips[i].y << ", "
+              << gips[i].z << ", "
+              << gips[i].w << "\n";
+  }
+  std::cout << "THERE\n";
 
   /* GLuint worldLoc = glGetUniformLocation(gp.program_id, "world");
 
